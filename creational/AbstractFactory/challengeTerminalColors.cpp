f33ce1ffbdac6ddec26159026391ac6bda29a05b
Luis Díaz Más <piponazo@gmail.com>
2022-04-14 14:01:52 +0200

    Use std::string instead of char[] and smart pointers

--- creational/AbstractFactory/challengeTerminalColors.cpp
+++ creational/AbstractFactory/challengeTerminalColors.cpp
@@ -4,37 +4,32 @@
 using namespace std;
 
 #pragma region Backgrounds
-// Base Background class
 class Background {
  protected:
-  char _theme[10];
+  std::string _theme;
 
  public:
-  Background() {
-  }
   virtual void getTheme() = 0;
 };
 
-// Derived Light Background class
 class LightBkgd : public Background {
  public:
   LightBkgd(HANDLE hConsole) {
     SetConsoleTextAttribute(hConsole, 240);
     cout << "Making the background white" << endl;
-    strcpy_s(_theme, "light");
+    _theme = "light";
   }
   void getTheme() {
     cout << "This is a part of the light theme" << endl;
   }
 };
 
-// Derived Dark Background class
 class DarkBkgd : public Background {
  public:
   DarkBkgd(HANDLE hConsole) {
     SetConsoleTextAttribute(hConsole, 15);
     cout << "Making the background black" << endl;
-    strcpy_s(_theme, "dark");
+    _theme = "dark";
   }
   void getTheme() {
     cout << "This is a part of the dark theme" << endl;
@@ -46,7 +41,7 @@ class DarkBkgd : public Background {
 // Base Text class
 class Text {
  protected:
-  char _theme[10];
+  std::string _theme;
 
  public:
   Text() {
@@ -59,7 +54,7 @@ class BlackText : public Text {
  public:
   BlackText(HANDLE hConsole) {
     SetConsoleTextAttribute(hConsole, 240);
-    strcpy_s(_theme, "light");
+    _theme = "light";
     cout << "Making the text black" << endl;
   }
   void message(HANDLE hConsole) {
@@ -73,7 +68,7 @@ class WhiteText : public Text {
  public:
   WhiteText(HANDLE hConsole) {
     SetConsoleTextAttribute(hConsole, 15);
-    strcpy_s(_theme, "dark");
+    _theme = "dark";
     cout << "Making the text white" << endl;
   }
   void message(HANDLE hConsole) {
@@ -92,29 +87,29 @@ class WhiteText : public Text {
 // Base Factory
 class ThemeFactory {
  public:
-  virtual Background* setBackground(HANDLE hConsole) = 0;
-  virtual Text* getDefaultText(HANDLE hConsole) = 0;
+  virtual std::unique_ptr<Background> setBackground(HANDLE hConsole) = 0;
+  virtual std::unique_ptr<Text> getDefaultText(HANDLE hConsole) = 0;
 };
 
 // Derived Light Theme Factory which pairs the white background with darker text
 class LightFactory : public ThemeFactory {
  public:
-  Background* setBackground(HANDLE hConsole) {
-    return new LightBkgd(hConsole);
+  std::unique_ptr<Background> setBackground(HANDLE hConsole) override {
+    return std::make_unique<LightBkgd>(hConsole);
   }
-  Text* getDefaultText(HANDLE hConsole) {
-    return new BlackText(hConsole);
+  std::unique_ptr<Text> getDefaultText(HANDLE hConsole) override {
+    return std::make_unique<BlackText>(hConsole);
   }
 };
 
 // Derived Dark Theme Factory which pairs the black background with lighter text
 class DarkFactory : public ThemeFactory {
  public:
-  Background* setBackground(HANDLE hConsole) {
-    return new DarkBkgd(hConsole);
+  std::unique_ptr<Background> setBackground(HANDLE hConsole) override {
+    return std::make_unique<DarkBkgd>(hConsole);
   }
-  Text* getDefaultText(HANDLE hConsole) {
-    return new WhiteText(hConsole);
+  std::unique_ptr<Text> getDefaultText(HANDLE hConsole) override {
+    return std::make_unique<WhiteText>(hConsole);
   }
 };
 #pragma endregion Factories
@@ -125,7 +120,7 @@ int main() {
   hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
   SetConsoleTextAttribute(hConsole, 30);
 
-  ThemeFactory* app;
+  std::unique_ptr<ThemeFactory> app;
   int choice;
 
   cout << "Select a theme: " << endl;
@@ -138,21 +133,20 @@ int main() {
   // Initiate the correct factory based on user input
   switch (choice) {
     case 1:
-      app = new LightFactory;
+      app = std::make_unique<LightFactory>();
       break;
     case 2:
-      app = new DarkFactory;
+      app = std::make_unique<DarkFactory>();
       break;
     default:
       cout << "Invalid Selection" << endl;
-      app = NULL;
       break;
   }
 
   // Get the correct objects based on factory selected
-  if (app != NULL) {
-    Background* appBkgd = app->setBackground(hConsole);
-    Text* appDefaultText = app->getDefaultText(hConsole);
+  if (app) {
+    auto appBkgd = app->setBackground(hConsole);
+    auto appDefaultText = app->getDefaultText(hConsole);
 
     appDefaultText->message(hConsole);
   }
